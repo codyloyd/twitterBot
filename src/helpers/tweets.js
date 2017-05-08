f29dfc24709c8540bcee4233434b5fa93c8adb5a
Cody Loyd <codyloyd@gmail.com>
2017-05-08 08:10:57 -0500

    update tweets

--- src/helpers/tweets.js
+++ src/helpers/tweets.js
@@ -2,18 +2,21 @@ module.exports = {
   promotion: [
     "This is the website we wish we'd had when we were learning to code on our own... http://www.theodinproject.com/",
     'come say hi in our chatroom! https://gitter.im/TheOdinProject/theodinproject',
-    'HERE COMES RAGNAROK: https://medium.com/the-odin-project/here-comes-ragnarok-a23744f9dcb9#.msc17ra63',
     "Have you always wanted to learn to code? Here's your chance. (It's FREE)http://www.theodinproject.com/",
     'The Odin Project is a FREE, Open Source curriculum for learning web development. Check us out! http://www.theodinproject.com/',
     'Check out the student solutions to this project: http://www.theodinproject.com/courses/web-development-101/lessons/javascript-and-jquery',
-    'DID YOU KNOW: most of our tweets are scheduled ahead of time by a bot created by one of our students. NICE! http://www.theodinproject.com/',
-    "Our curriculum is completely open source.  See something that's out of date? Help us out! https://github.com/TheOdinProject/curriculum"
+    'DID YOU KNOW: most of our tweets are scheduled ahead of time by a bot created by one of our students. NICE! https://github.com/codyloyd/twitterBot',
+    "Our curriculum is completely open source.  See something that's out of date? Help us out! https://github.com/TheOdinProject/curriculum",
+    'There is no reason NOT to start today: https://www.theodinproject.com/courses/introduction-to-web-development',
+    'Our focus is on giving students the most relevant, employable real-world coding skills. It goes way beyond memorizing syntax.',
+    'Changing careers? We provide you with what you need to know to get a job.',
+    "Come join us in the Odin chatroom!  There's always someone hanging around if you need some help. https://gitter.im/TheOdinProject"
   ],
   articles: [
-    "You need to read this if you haven't already. https://medium.com/the-odin-project/learning-to-code-when-youre-really-dang-busy-e223a7f84758",
     'Introduction to DESIGN PATTERNS (in Ruby) http://www.chadkreutzer.com/crack-in-the-code/coding/Introduction-to-Design-Patterns.html',
     'Odin redesign underway: https://medium.com/the-odin-project/here-comes-ragnarok-a23744f9dcb9',
     'Why not follow us on Medium? https://medium.com/the-odin-project',
-    'Use it or Lose it: https://medium.com/@codyloyd/use-it-or-lose-it-121f15b8cbe1'
+    'Use it or Lose it: https://medium.com/@codyloyd/use-it-or-lose-it-121f15b8cbe1',
+    'Are YOU a coding valkyrie? https://medium.com/the-odin-project/are-you-a-coding-valkyrie-576979504d72'
   ]
-};
+}
